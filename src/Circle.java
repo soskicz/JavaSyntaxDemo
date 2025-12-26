b02f7d6fdf976e5f177bbd8b60c57e4febf67dd5
Inforce Academy <zsoskic.ia@gmail.com>
2025-12-26 23:46:55 +0100

    Demos for classes completed

--- src/Circle.java
+++ src/Circle.java
@@ -1,31 +1,51 @@
 class Circle {
+    static double PI = 3.141592;
+
     double radius;
     int color;
+
+    Circle() {
+        this.radius = 0.0;
+        this.color = 0x000000;
+    }
+
+    public Circle(double radius, int color) {
+        this.radius = radius;
+        this.color = color;
+    }
 
     int getColor() {
         return this.color;
     }
-
-    int setColor(int color) {
-        this.color = color;
-        return color;
+    void setColor(int c) {
+        this.color = c;
     }
 
-    void setColorVoid(int c) {
-        this.color = c;
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    double calculateArea() {
+        return Circle.PI * this.radius * this.radius;
     }
 
     public static void main(String[] args) {
         Circle Left, Right, Middle;
 
-        Left = new Circle();
-        Right = new Circle();
-        Middle = new Circle();
-
-        Left = Right = Middle = new Circle();
-
+        Left = new Circle(); // default constructor
         Left.radius = 0.5;
-        Middle.radius = 1.0;
-        Right.radius = 0.5;
+        Left.setColor(0xFF0000);
+
+        Middle = new Circle(5.0,0xFF0000);
+        Right = new Circle(10.0, 0x0000FF);
+
+        int colorInTheMiddle = Middle.getColor();
+        double areaRight = Right.calculateArea();
+
+        System.out.println("The color of the circle in the middle is " + colorInTheMiddle);
+        System.out.println("The area of the right circle is " + areaRight);
     }
 }
